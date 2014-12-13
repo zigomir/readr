7e9001edb52e8cee9b7a6d866268b79eea7e8684
zigomir <zigomir@gmail.com>
2014-12-13 20:42:42 +0100

    FP style

--- tests/lib/word-counter-spec.js
+++ tests/lib/word-counter-spec.js
@@ -2,14 +2,14 @@ var wordCounter = require('../../lib/word-counter');
 
 describe('word counter', function() {
   it('should count words', function() {
-    var result = wordCounter('hudascena hudascena fkyea fkyea fkyea'.split(' '));
+    var result = wordCounter('hudascena hudascena fkyea fkyea fkyea'.split(/\W+/));
 
     expect(result[0]).toEqual({ word: 'fkyea', count: 3 });
     expect(result[1]).toEqual({ word: 'hudascena', count: 2 });
   });
 
   it('should not count too short words', function() {
-    var result = wordCounter('lol lol lol'.split(' '));
+    var result = wordCounter('lol lol lol'.split(/\W+/));
     expect(result).toEqual([]);
   });
 });
